ba375c58e6538a7b311809acaba85906c49c950e
Rawan Bazadough <bazadough1997@gmail.com>
2020-10-15 00:38:35 +0300

    project-done

--- signup/signup.js
+++ signup/signup.js
@@ -1,17 +1,24 @@
 document.getElementById("alert").addEventListener("click", checkF);
 
+var doErrMsg=false;
+
 function checkF(){
 var myinput = document.getElementById("pwd");
 if (myinput.value.length >= 8){
 	myinput.style.backgroundColor = "white";
 	myinput.style.color= "black";
+	document.getElementById("signup").onclick = function () {
+        location.href = "C:/Users/DELL/Desktop/RBK-Website/login/login.html";}
 } else {
 	myinput.style.backgroundColor = "red";
 	myinput.style.color= "yellow";
+	if(doErrMsg==false)
+	{
+		var x = myinput.insertAdjacentText("afterend", "invalid password");
+		doErrMsg=true;
+	}
 }
 }
-
-
 
 
 
